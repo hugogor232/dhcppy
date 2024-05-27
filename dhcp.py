923e9a305e2fec9f9ddfd0c04c54038f672f8a56
hugogor232 <83557013+hugogor232@users.noreply.github.com>
2024-05-27 16:05:01 +0200

    Add files via upload

--- dhcp.py
+++ dhcp.py
@@ -1,5 +1,5 @@
 
-dhcp_serveur = 'INTERFACESv4="eth3"'
+dhcp_serveur = 'INTERFACESv4="'+str(input('nom interface (ens192) : '))+'"'
 dhcp_conf = 'default-lease-time 600;\nmax-lease-time 7200;\noption subnet-mask '+str(input('masque : '))+';\noption routers '+str(input('routeur par default attribue : '))+';\noption domain-name-servers '+str(input('DNS attribue : '))+';\nsubnet '+str(input('reseau : '))+' netmask '+str(input('masque : '))+' {\n range '+str(input('range (x.x.x.x x.x.x.x)'))+'; \n}'
 
 f = open("/etc/default/isc-dhcp-server", "w")
